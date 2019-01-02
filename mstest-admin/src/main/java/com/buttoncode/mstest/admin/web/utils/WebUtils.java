c7256e52489bfc14215b426e115452d4375fbbc3
Jaroslaw <guzik.jaroslaw@gmail.com>
2019-01-02 08:44:01 +0100

    Compliance 1.5 - Service

--- mstest-admin/src/main/java/com/buttoncode/mstest/admin/web/utils/WebUtils.java
+++ mstest-admin/src/main/java/com/buttoncode/mstest/admin/web/utils/WebUtils.java
@@ -19,6 +19,16 @@ public class WebUtils
 	public static final String FILE_PDF_TASKBOOK_URL = ADDRESS_CONNECT_TO_SERVER + FOLDER_NAME_FILES_ACTIVE + "task_book/";
 	public static final String FILE_PDF_TASKBOOK_DELETED_URL = ADDRESS_CONNECT_TO_SERVER + FOLDER_NAME_FILES_DELETE + "task_book/";
 
+	public static final String FILE_PDF_COMPLIANCE_ANTICORRUPTION_POLICY_URL = ADDRESS_CONNECT_TO_SERVER + FOLDER_NAME_FILES_ACTIVE + "anticorruption_policy/";
+	public static final String FILE_PDF_COMPLIANCE_ANTICORRUPTION_POLICY_DELETED_URL = ADDRESS_CONNECT_TO_SERVER + FOLDER_NAME_FILES_DELETE + "anticorruption_policy/";
+
+	public static final String IMAGES_COMPLIANCE_ANTICORRUPTION_POLICY = "_ComplianceAnticorruptionPolicy_";
+	public static final String IMAGES_COMPLIANCE_CODE_OF_ETHIC = "_ComplianceCodeofEthic_";
+
+	public static final String FILE_PDF_COMPLIANCE_CODE_OF_ETHIC_URL = ADDRESS_CONNECT_TO_SERVER + FOLDER_NAME_FILES_ACTIVE + "code_of_ethic/";
+	public static final String FILE_PDF_COMPLIANCE_CODE_OF_ETHIC_DELETED_URL = ADDRESS_CONNECT_TO_SERVER + FOLDER_NAME_FILES_DELETE + "code_of_ethic/";
+
+
 	public static final String IMAGES_TASKBOOK = "_TaskBook_";
 
 	public static String getURLWithContextPath(HttpServletRequest request)
